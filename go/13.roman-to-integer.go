00ef87a6db12690239a89f0fe17831c044f385c2
Tai <letientai299@gmail.com>
2019-03-11 15:51:41 +0800

    Done 13

---
+++ go/13.roman-to-integer.go
@@ -0,0 +1,114 @@
+package main
+
+/*
+ * @lc app=leetcode id=13 lang=golang
+ *
+ * [13] Roman to Integer
+ *
+ * https://leetcode.com/problems/roman-to-integer/description/
+ *
+ * algorithms
+ * Easy (51.56%)
+ * Total Accepted:    372K
+ * Total Submissions: 720.1K
+ * Testcase Example:  '"III"'
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+ * and M.
+ *
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * For example, two is written as II in Roman numeral, just two one's added
+ * together. Twelve is written as, XII, which is simply X + II. The number
+ * twenty seven is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is
+ * written as IV. Because the one is before the five we subtract it making
+ * four. The same principle applies to the number nine, which is written as IX.
+ * There are six instances where subtraction is used:
+ *
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9. 
+ * X can be placed before L (50) and C (100) to make 40 and 90. 
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ *
+ * Given a roman numeral, convert it to an integer. Input is guaranteed to be
+ * within the range from 1 to 3999.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "III"
+ * Output: 3
+ *
+ * Example 2:
+ *
+ *
+ * Input: "IV"
+ * Output: 4
+ *
+ * Example 3:
+ *
+ *
+ * Input: "IX"
+ * Output: 9
+ *
+ * Example 4:
+ *
+ *
+ * Input: "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input: "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ */
+func romanToInt(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+
+	symbols := map[uint8]int{
+		uint8("I"[0]): 1,
+		uint8("V"[0]): 5,
+		uint8("X"[0]): 10,
+		uint8("L"[0]): 50,
+		uint8("C"[0]): 100,
+		uint8("D"[0]): 500,
+		uint8("M"[0]): 1000,
+	}
+	res := 0
+	sign := 1
+	last := symbols[s[n-1]]
+	res += last
+	for i := n - 2; i >= 0; i-- {
+		current := symbols[s[i]]
+		if current < last {
+			sign = -1
+		} else if current > last {
+			sign = 1
+		}
+
+		res += sign * current
+		last = current
+	}
+
+	return res
+}
---
+++ go/13.roman-to-integer_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func Test_romanToInt(t *testing.T) {
+	tests := []struct {
+		in   string
+		want int
+	}{
+		{"I", 1},
+		{"II", 2},
+		{"III", 3},
+		{"V", 5},
+		{"IV", 4},
+		{"VI", 6},
+		{"VII", 7},
+		{"VIII", 8},
+		{"IX", 9},
+		{"X", 10},
+		{"XI", 11},
+		{"LVIII", 58},
+		{"MCMXCIV", 1994},
+	}
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			if got := romanToInt(tt.in); got != tt.want {
+				t.Errorf("romanToInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
