6a0b43a799e824f6607d4bec8fb0f0ee3c7d4a4d
Tai <tai.le@shopee.com>
2019-03-11 12:45:48 +0800

    Done add_two_numbers at 100% speed

---
+++ add_two_numbers/add_two_number.go
@@ -0,0 +1,71 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	n := new(ListNode)
+	root := n
+
+	x, y, r := l1, l2, 0
+
+	for {
+		t := x.Val + y.Val + r
+		n.Val = t % 10
+		r = t / 10
+		x = x.Next
+		y = y.Next
+		if x == nil || y == nil {
+			break
+		}
+		n.Next = new(ListNode)
+		n = n.Next
+	}
+
+	var z *ListNode
+	if x == nil {
+		z = y
+	}
+
+	if y == nil {
+		z = x
+	}
+
+	if z != nil {
+		n.Next = new(ListNode)
+		n = n.Next
+
+		for z != nil {
+			t := z.Val + r
+			r = t / 10
+			n.Val = t % 10
+			if z.Next != nil {
+				n.Next = new(ListNode)
+				n = n.Next
+				z = z.Next
+			} else {
+				break
+			}
+		}
+	}
+
+	if r != 0 {
+		n.Next = new(ListNode)
+		n.Next.Val = r
+	}
+
+	return root
+}
---
+++ add_two_numbers/add_two_number_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func (n *ListNode) String() string {
+	buf := strings.Builder{}
+	for {
+		buf.WriteString(fmt.Sprintf("%d", n.Val))
+		if n.Next != nil {
+			n = n.Next
+			buf.WriteString(" --> ")
+		} else {
+			break
+		}
+	}
+
+	return buf.String()
+}
+
+func Test_addTwoNumbers(t *testing.T) {
+	tests := []struct {
+		x, y int
+	}{
+		{
+			x: 12345,
+			y: 300,
+		},
+		{
+			x: 0,
+			y: 300,
+		},
+		{
+			x: 12345,
+			y: 98765,
+		},
+		{
+			x: 243,
+			y: 564,
+		},
+	}
+	toList := func(x int) *ListNode {
+		root := new(ListNode)
+		n := root
+		for {
+			n.Val = x % 10
+			x = x / 10
+			if x > 0 {
+				n.Next = new(ListNode)
+				n = n.Next
+			} else {
+				break
+			}
+		}
+		return root
+	}
+
+	toInt := func(n *ListNode) int {
+		v := 0
+		p := 1
+		for n != nil {
+			v = v + p*n.Val
+			p = p * 10
+			n = n.Next
+		}
+		return v
+	}
+
+	for _, tt := range tests {
+		z := tt.x + tt.y
+		name := fmt.Sprintf("%d + %d == %d", tt.x, tt.y, z)
+		t.Run(name, func(t *testing.T) {
+			l1 := toList(tt.x)
+			fmt.Printf("%13d | %s\n", toInt(l1), l1)
+			l2 := toList(tt.y)
+			fmt.Printf("%13d | %s\n", toInt(l2), l2)
+			got := addTwoNumbers(l1, l2)
+			actual := toInt(got)
+			fmt.Printf("%5d ~ %5d | %s\n", z, actual, got)
+			if actual != z {
+				t.Errorf("addTwoNumbers() = %v, want %v", actual, z)
+			}
+		})
+	}
+}
