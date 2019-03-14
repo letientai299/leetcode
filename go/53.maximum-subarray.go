b4eb9c0ba266ef6c95f77ed96abbc1ab6237676a
Tai <letientai299@gmail.com>
2019-03-14 15:27:25 +0800

    Done 53,  but not by myself

---
+++ go/53.maximum-subarray.go
@@ -0,0 +1,53 @@
+package main
+
+/*
+ * @lc app=leetcode id=53 lang=golang
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (42.89%)
+ * Total Accepted:    473.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ *
+ * Example:
+ *
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ *
+ * Follow up:
+ *
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ *
+ */
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	max := nums[0]
+	cur := max
+
+	for i := 1; i < len(nums); i++ {
+		if cur > 0 {
+			cur = cur + nums[i]
+		} else {
+			cur = nums[i]
+		}
+
+		if cur > max {
+			max = cur
+		}
+	}
+	return max
+}
---
+++ go/53.maximum-subarray_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func Test_maxSubArray(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want int
+	}{
+		{
+			nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+			want: 6,
+		},
+		{[]int{6}, 6},
+		{[]int{6,-1}, 6},
+		{[]int{6,1}, 7},
+		{[]int{}, 0},
+		{[]int{3,2,1,-7,4,5,-2}, 9},
+		{[]int{3,2,1,-2,4,5,-2}, 13},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := maxSubArray(tt.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
