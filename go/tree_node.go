08865d3eae459aeb010767fc351ca5634be56e3a
Tai <letientai299@gmail.com>
2019-04-03 02:17:58 +0800

    Add levelOrder

--- go/tree_node.go
+++ go/tree_node.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
+
+const NA = math.MaxInt32
 
 type TreeNode struct {
 	Val   int       `json:"val"`
@@ -21,6 +24,36 @@ func (t *TreeNode) inOrder() []int {
 	return append(result, t.Right.inOrder()...)
 }
 
+func (t *TreeNode) levelOrder() []int {
+	if t == nil {
+		return []int{NA}
+	}
+
+	q := make([]*TreeNode, 1)
+	var result []int = nil
+	q[0] = t
+	for len(q) != 0 {
+		nextQ := make([]*TreeNode, 0)
+		for _, n := range q {
+			if n == nil {
+				result = append(result, NA)
+				continue
+			}
+			result = append(result, n.Val)
+			nextQ = append(nextQ, n.Left)
+			nextQ = append(nextQ, n.Right)
+		}
+		q = nextQ
+	}
+
+	i := len(result) - 1
+	for result[i] == NA {
+		i--
+	}
+
+	return result[:i+1]
+}
+
 func treeFromJson(str string) *TreeNode {
 	decoder := json.NewDecoder(strings.NewReader(str))
 	tr := new(TreeNode)
@@ -30,4 +63,43 @@ func treeFromJson(str string) *TreeNode {
 	}
 
 	return tr
+}
+
+func treeFromLevelOrder(values ...int) *TreeNode {
+	if len(values) == 0 {
+		return nil
+	}
+
+	nodes := make([]*TreeNode, 1)
+	root := &TreeNode{
+		Val: values[0],
+	}
+	nodes[0] = root
+	i := 1
+	for len(nodes) != 0 && i < len(values) {
+		// get the first node of the queue
+		node := nodes[0]
+
+		// make new nodes and append to the queue
+		if i < len(values) && values[i] != NA {
+			// this node has value
+			node.Left = &TreeNode{
+				Val: values[i],
+			}
+			nodes = append(nodes, node.Left)
+		}
+		i++
+
+		if i < len(values) && values[i] != NA {
+			// this node has value
+			node.Right = &TreeNode{
+				Val: values[i],
+			}
+			nodes = append(nodes, node.Right)
+		}
+		i++
+		nodes = nodes[1:]
+	}
+
+	return root
 }
---
+++ go/tree_node_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_treeFromLevelOrder(t *testing.T) {
+	tests := []struct {
+		values []int
+	}{
+		{[]int{10, 5}},
+		{[]int{10, 5, 3}},
+		{[]int{10, 5, 3, 1}},
+		{[]int{10, 5, -3, 3, 2, NA, 11, 3, -2, NA, 1}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			tree := treeFromLevelOrder(tt.values...);
+			levelOderAgain := tree.levelOrder()
+
+			if !reflect.DeepEqual(levelOderAgain, tt.values) {
+				t.Errorf("treeFromLevelOrder() = %v, want %v", levelOderAgain, tt.values)
+			}
+		})
+	}
+}
