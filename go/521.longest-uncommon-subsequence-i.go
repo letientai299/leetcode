97b4b8e6b7409455007f485b93cdc2d527a2b7ae
Tai <letientai299@gmail.com>
2019-06-03 02:27:05 +0800

    Done go 521

---
+++ go/521.longest-uncommon-subsequence-i.go
@@ -0,0 +1,68 @@
+package main
+
+/*
+ * @lc app=leetcode id=521 lang=golang
+ *
+ * [521] Longest Uncommon Subsequence I
+ *
+ * https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+ *
+ * algorithms
+ * Easy (56.39%)
+ * Total Accepted:    46.7K
+ * Total Submissions: 82.7K
+ * Testcase Example:  '"aba"\n"cdc"'
+ *
+ *
+ * Given a group of two strings, you need to find the longest uncommon
+ * subsequence of this group of two strings.
+ * The longest uncommon subsequence is defined as the longest subsequence of
+ * one of these strings and this subsequence should not be any subsequence of
+ * the other strings.
+ *
+ *
+ *
+ * A subsequence is a sequence that can be derived from one sequence by
+ * deleting some characters without changing the order of the remaining
+ * elements. Trivially, any string is a subsequence of itself and an empty
+ * string is a subsequence of any string.
+ *
+ *
+ *
+ * The input will be two strings, and the output needs to be the length of the
+ * longest uncommon subsequence. If the longest uncommon subsequence doesn't
+ * exist, return -1.
+ *
+ *
+ * Example 1:
+ *
+ * Input: "aba", "cdc"
+ * Output: 3
+ * Explanation: The longest uncommon subsequence is "aba" (or "cdc"), because
+ * "aba" is a subsequence of "aba", but not a subsequence of any other strings
+ * in the group of two strings.
+ *
+ *
+ *
+ * Note:
+ *
+ * Both strings' lengths will not exceed 100.
+ * Only letters from a ~ z will appear in input strings.
+ *
+ *
+ */
+func findLUSlength(a string, b string) int {
+	if len(a) > len(b) {
+		return len(a)
+	}
+
+	if len(b) > len(a) {
+		return len(b)
+	}
+
+	if a == b {
+		return -1
+	}
+
+	return len(a)
+}
---
+++ go/521.longest-uncommon-subsequence-i_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_findLUSlength(t *testing.T) {
+	tests := []struct {
+		a    string
+		b    string
+		want int
+	}{
+		{
+			a:    "aba",
+			b:    "cdc",
+			want: 3,
+		},
+
+		{
+			a:    "aba",
+			b:    "adc",
+			want: 3,
+		},
+
+		{
+			a:    "aba",
+			b:    "abc",
+			want: 2,
+		},
+
+		{
+			a:    "aba",
+			b:    "cdc",
+			want: 3,
+		},
+	}
+	for _, tc := range tests {
+		tt := tc
+		testName := fmt.Sprintf(
+			"%v, %v",
+			tt.a, tt.b,
+		)
+		t.Run(testName, func(t *testing.T) {
+			got := findLUSlength(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("findLUSlength(%v, %v) = %v, want %v", tt.a, tt.b, got, tt.want)
+			}
+		})
+	}
+}
