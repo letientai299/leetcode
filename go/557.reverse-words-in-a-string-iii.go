de4ddae1c1a9b11ec64ba988457ccb29de91521d
Tai <letientai299@gmail.com>
2019-06-10 09:32:55 +0800

    Done go 557

---
+++ go/557.reverse-words-in-a-string-iii.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+ * @lc app=leetcode id=557 lang=golang
+ *
+ * [557] Reverse Words in a String III
+ *
+ * https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+ *
+ * algorithms
+ * Easy (64.14%)
+ * Total Accepted:    127.1K
+ * Total Submissions: 198K
+ * Testcase Example:  `"Let's take LeetCode contest"`
+ *
+ * Given a string, you need to reverse the order of characters in each word
+ * within a sentence while still preserving whitespace and initial word order.
+ *
+ * Example 1:
+ *
+ * Input: "Let's take LeetCode contest"
+ * Output: "s'teL ekat edoCteeL tsetnoc"
+ *
+ *
+ *
+ * Note:
+ * In the string, each word is separated by single space and there will not be
+ * any extra space in the string.
+ *
+ */
+func reverseWords(s string) string {
+	buf := []byte(s)
+	rev := func(buf []byte, left, right int) {
+		n := right - left
+		if n < 0 {
+			return
+		}
+		for i := 0; i <= n/2; i++ {
+			buf[i+left], buf[right-i] = buf[right-i], buf[left+i]
+		}
+	}
+	left := 0
+	for i := 1; i < len(buf); i++ {
+		if buf[i] == ' ' && buf[i-1] != ' ' {
+			rev(buf, left, i-1)
+			continue
+		}
+
+		if buf[i] != ' ' && buf[i-1] == ' ' {
+			left = i
+		}
+	}
+	rev(buf, left, len(buf)-1)
+	return string(buf)
+}
---
+++ go/557.reverse-words-in-a-string-iii_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_reverseWords(t *testing.T) {
+	tests := []struct {
+		s    string
+		want string
+	}{
+		{
+			s:    "",
+			want: "",
+		},
+
+		{
+			s:    "Let's take LeetCode contest",
+			want: "s'teL ekat edoCteeL tsetnoc",
+		},
+
+		{
+			s:    "hello world",
+			want: "olleh dlrow",
+		},
+		{
+			s:    "a b c",
+			want: "a b c",
+		},
+	}
+	for _, tc := range tests {
+		tt := tc
+		testName := fmt.Sprintf(
+			"%v",
+			tt.s,
+		)
+		t.Run(testName, func(t *testing.T) {
+			got := reverseWords(tt.s)
+			if got != tt.want {
+				t.Errorf("reverseWords(%v) = %v, want %v", tt.s, got, tt.want)
+			}
+		})
+	}
+}
