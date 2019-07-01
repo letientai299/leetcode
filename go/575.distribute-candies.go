567f0a4eda35d50f9d4164d9e84286b29c0ed8ef
Tai <letientai299@gmail.com>
2019-07-02 03:29:10 +0800

    Done go 575

---
+++ go/575.distribute-candies.go
@@ -0,0 +1,66 @@
+package main
+
+/*
+ * @lc app=leetcode id=575 lang=golang
+ *
+ * [575] Distribute Candies
+ *
+ * https://leetcode.com/problems/distribute-candies/description/
+ *
+ * algorithms
+ * Easy (59.64%)
+ * Total Accepted:    73.7K
+ * Total Submissions: 123.3K
+ * Testcase Example:  '[1,1,2,2,3,3]'
+ *
+ * Given an integer array with even length, where different numbers in this
+ * array represent different kinds of candies. Each number means one candy of
+ * the corresponding kind. You need to distribute these candies equally in
+ * number to brother and sister. Return the maximum number of kinds of candies
+ * the sister could gain.
+ *
+ * Example 1:
+ *
+ * Input: candies = [1,1,2,2,3,3]
+ * Output: 3
+ * Explanation:
+ * There are three different kinds of candies (1, 2 and 3), and two candies for
+ * each kind.
+ * Optimal distribution: The sister has candies [1,2,3] and the brother has
+ * candies [1,2,3], too.
+ * The sister has three different kinds of candies.
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: candies = [1,1,2,3]
+ * Output: 2
+ * Explanation: For example, the sister has candies [2,3] and the brother has
+ * candies [1,1].
+ * The sister has two different kinds of candies, the brother has only one kind
+ * of candies.
+ *
+ *
+ *
+ * Note:
+ *
+ * The length of the given array is in range [2, 10,000], and will be even.
+ * The number in given array is in range [-100,000, 100,000].
+ *
+ *
+ */
+func distributeCandies(candies []int) int {
+	types := make(map[int]struct{}, len(candies))
+	for _, v := range candies {
+		types[v] = struct{}{}
+	}
+
+	n := len(candies) / 2
+	m := len(types)
+	if m < n {
+		return m
+	}
+
+	return n
+}
---
+++ go/575.distribute-candies_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_distributeCandies(t *testing.T) {
+	tests := []struct {
+		candies []int
+		want    int
+	}{
+		{candies: []int{1, 2}, want: 1},
+		{candies: []int{1, 1}, want: 1},
+		{candies: []int{1, 2, 1, 2}, want: 2},
+		{candies: []int{1, 1, 1, 2, 1, 3, 1, 4}, want: 4},
+		{candies: []int{1, 1, 3, 2}, want: 2},
+		{candies: []int{1, 2, 3, 4}, want: 2},
+		{candies: []int{1, 1, 1, 4}, want: 2},
+		{candies: []int{1, 1, 1, 1}, want: 1},
+		{candies: []int{1, 1, 2, 2, 3, 3}, want: 3},
+	}
+	for _, tc := range tests {
+		tt := tc
+		testName := fmt.Sprintf(
+			"%v",
+			tt.candies,
+		)
+		t.Run(testName, func(t *testing.T) {
+			got := distributeCandies(tt.candies)
+			if got != tt.want {
+				t.Errorf("distributeCandies(%v) = %v, want %v", tt.candies, got, tt.want)
+			}
+		})
+	}
+}
