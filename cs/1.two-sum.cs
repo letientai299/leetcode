ceabc83431ef359d7c5a28c24a5fc8ab6e435a17
Tai <letientai299@gmail.com>
2019-07-07 16:43:28 +0800

    Init c# project

---
+++ cs/1.two-sum.cs
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=1 lang=csharp
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (42.01%)
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.6M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System;
+
+public class Solution1
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        return new int[] { };
+    }
+}
