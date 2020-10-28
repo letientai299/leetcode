b1960b557eb71e0693a47c1a1b6e2212c12c0055
Tai <letientai299@gmail.com>
2020-10-29 04:54:45 +0800

    DOne go 276

---
+++ go/276.paint-fence.go
@@ -0,0 +1,68 @@
+package main
+
+/*
+ * @lc app=leetcode id=276 lang=golang
+ *
+ * [276] Paint Fence
+ *
+ * https://leetcode.com/problems/paint-fence/description/
+ *
+ * algorithms
+ * Easy (37.24%)
+ * Total Accepted:    59.3K
+ * Total Submissions: 153.6K
+ * Testcase Example:  '3\n2'
+ *
+ * There is a fence with n posts, each post can be painted with one of the k
+ * colors.
+ *
+ * You have to paint all the posts such that no more than two adjacent fence
+ * posts have the same color.
+ *
+ * Return the total number of ways you can paint the fence.
+ *
+ * Note:
+ * n and k are non-negative integers.
+ *
+ * Example:
+ *
+ *
+ * Input: n = 3, k = 2
+ * Output: 6
+ * Explanation: Take c1 as color 1, c2 as color 2. All possible ways
+ * are:
+ *
+ *            post1  post2  post3
+ * -----      -----  -----  -----
+ *   1         c1     c1     c2
+ *   2         c1     c2     c1
+ *   3         c1     c2     c2
+ *   4         c2     c1     c1
+ *   5         c2     c1     c2
+ *   6         c2     c2     c1
+ *
+ *
+ */
+func numWays(n int, k int) int {
+	dp1 := k
+	dp2 := k * k
+
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return dp1
+	case 2:
+		return dp2
+	}
+
+	for ; n > 2; n-- {
+		// i-3 | i-2 | i-1  | ... |
+		// dp0 | dp1 | dp2 |
+		v := (k-1)*dp2 + // i has different color with i-1
+			+ (k-1)*dp1 // i has different color with i-2
+		dp1, dp2 = dp2, v
+	}
+
+	return dp2
+}
---
+++ go/276.paint-fence_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_numWays(t *testing.T) {
+	tests := []struct {
+		n    int
+		k    int
+		want int
+	}{
+		// aab, aba, abb, bab, bba, baa
+		{n: 3, k: 2, want: 6},
+
+		{n: 2, k: 1, want: 1},
+
+		{n: 1, k: 2, want: 2},
+
+		// __aa: _(b|c)aa
+		// __bb: _(
+		// __cc:
+		{n: 4, k: 3, want: 66},
+		// aab, aac, bba, bbc, cca, ccb
+		// abb, aba, abc
+		// aca, acb, acc
+		// baa, bab, bac
+		// bca, bcb, bcc
+		// caa, cab, cac
+		// cba, cbb, cbc
+		{n: 3, k: 3, want: 24},
+
+		// aa, ab, ac,
+		// bb, ba, bc
+		// cc, ca, cb
+		{n: 2, k: 3, want: 9},
+		{n: 1, k: 3, want: 3},
+
+		// aaba, abaa, abba, baba, bbaa, baab
+		// aabb, abab, babb, bbab,
+		{n: 4, k: 2, want: 10},
+		// aa, ab, bb, ba
+		{n: 2, k: 2, want: 4},
+		{n: 3, k: 1, want: 0},
+		{n: 1, k: 1, want: 1},
+	}
+	for _, tc := range tests {
+		tt := tc
+		testName := fmt.Sprintf(
+			"%v, %v",
+			tt.n, tt.k,
+		)
+		t.Run(testName, func(t *testing.T) {
+			got := numWays(tt.n, tt.k)
+			if got != tt.want {
+				t.Errorf("numWays(%v, %v) = %v, want %v", tt.n, tt.k, got, tt.want)
+			}
+		})
+	}
+}
