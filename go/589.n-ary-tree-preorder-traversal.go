d5df16b975f32c084c1e425cb723756b2d30fda9
Tai <letientai299@gmail.com>
2020-10-30 16:52:23 +0800

    Done go 589

---
+++ go/589.n-ary-tree-preorder-traversal.go
@@ -0,0 +1,79 @@
+package main
+
+/*
+ * @lc app=leetcode id=589 lang=golang
+ *
+ * [589] N-ary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (69.43%)
+ * Total Accepted:    122.2K
+ * Total Submissions: 168.1K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * Given an n-ary tree, return the preorder traversal of its nodes' values.
+ *
+ * Nary-Tree input serialization is represented in their level order traversal,
+ * each group of children is separated by the null value (See examples).
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Recursive solution is trivial, could you do it iteratively?
+ *
+ *
+ * Example 1:
+ *
+ *
+ *
+ *
+ * Input: root = [1,null,3,2,4,null,5,6]
+ * Output: [1,3,5,6,2,4]
+ *
+ *
+ * Example 2:
+ *
+ *
+ *
+ *
+ * Input: root =
+ * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ * Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The height of the n-ary tree is less than or equal to 1000
+ * The total number of nodes is between [0, 10^4]
+ *
+ *
+ */
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func preorder(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+
+	stack := []*Node{root}
+	var res []int
+
+	for len(stack) != 0 {
+		n := stack[0]
+		stack = append(n.Children, stack[1:]...)
+		res = append(res, n.Val)
+	}
+
+	return res
+}
