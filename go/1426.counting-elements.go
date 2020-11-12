81dd14b39591139960c92a40f109f4d078ded381
Tai <letientai299@gmail.com>
2020-11-12 19:09:51 +0800

    Done go 1426

---
+++ go/1426.counting-elements.go
@@ -0,0 +1,28 @@
+package main
+
+import "sort"
+
+func countElements(arr []int) int {
+	if len(arr) < 2 {
+		return 0
+	}
+
+	sort.Ints(arr)
+	s := 0
+
+	prevCount := 1
+	prev := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i] == prev {
+			prevCount++
+			continue
+		}
+
+		if arr[i] == prev+1 {
+			s += prevCount
+		}
+		prevCount = 1
+		prev = arr[i]
+	}
+	return s
+}
