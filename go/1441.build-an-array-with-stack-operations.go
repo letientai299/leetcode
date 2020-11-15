307fa5af66e9949e4fdad9461f200853d9f2a75b
Tai <letientai299@gmail.com>
2020-11-15 10:19:53 +0800

    Done go 1441

---
+++ go/1441.build-an-array-with-stack-operations.go
@@ -0,0 +1,16 @@
+package main
+
+func buildArray(target []int, n int) []string {
+	res := make([]string, 0, n-len(target))
+	last := 0
+	for _, x := range target {
+		for i := last + 1; i < x; i++ {
+			res = append(res, "Push")
+			res = append(res, "Pop")
+		}
+
+		res = append(res, "Push")
+		last = x
+	}
+	return res
+}
---
+++ go/1441.build-an-array-with-stack-operations_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildArray(t *testing.T) {
+	tests := []struct {
+		target []int
+		n      int
+		want   []string
+	}{
+		{target: []int{2, 3, 4}, n: 3, want: []string{"Push", "Pop", "Push", "Push", "Push"}},
+		{target: []int{2}, n: 3, want: []string{"Push", "Pop", "Push"}},
+		{target: []int{1, 2}, n: 3, want: []string{"Push", "Push"}},
+		{target: []int{1, 3}, n: 3, want: []string{"Push", "Push", "Pop", "Push"}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := buildArray(tt.target, tt.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
