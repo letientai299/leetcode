e992d376bb4e2863d3dc02705bafd6ddfda7e699
Tai <letientai299@gmail.com>
2020-12-08 15:07:56 +0800

    Done go 145

---
+++ go/145.binary-tree-postorder-traversal.go
@@ -0,0 +1,86 @@
+package main
+
+/*
+ * @lc app=leetcode id=145 lang=golang
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (51.21%)
+ * Total Accepted:    433.1K
+ * Total Submissions: 765.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [3,2,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: root = [1,2]
+ * Output: [2,1]
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: [2,1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Recursive solution is trivial, could you do it iteratively?
+ *
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	return append(postorderTraversal(root.Left), append(postorderTraversal(root.Right), []int{root.Val}...)...)
+}
