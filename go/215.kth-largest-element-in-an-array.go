07183ea2f527805c70b17ed1e4c60300a2af4c43
Tai <letientai299@gmail.com>
2020-12-09 03:01:50 +0800

    Done go 215

---
+++ go/215.kth-largest-element-in-an-array.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+ * @lc app=leetcode id=215 lang=golang
+ *
+ * [215] Kth Largest Element in an Array
+ *
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (51.02%)
+ * Total Accepted:    754.3K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * Find the kth largest element in an unsorted array. Note that it is the kth
+ * largest element in the sorted order, not the kth distinct element.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ *
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ *
+ */
+func findKthLargest(nums []int, k int) int {
+	return quickSelect(nums, len(nums)-k)
+}
