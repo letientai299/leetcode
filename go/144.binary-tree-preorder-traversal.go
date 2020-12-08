06f3cd37a7087d49b6328145539694687b17f12b
Tai <letientai299@gmail.com>
2020-12-08 15:06:21 +0800

    Done go 144

---
+++ go/144.binary-tree-preorder-traversal.go
@@ -0,0 +1,87 @@
+package main
+
+/*
+ * @lc app=leetcode id=144 lang=golang
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (53.22%)
+ * Total Accepted:    565.9K
+ * Total Submissions: 996.3K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the preorder traversal of its nodes'
+ * values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [1,2,3]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: root = [1,2]
+ * Output: [1,2]
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: [1,2]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Recursive solution is trivial, could you do it iteratively?
+ *
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	return append([]int{root.Val}, append(preorderTraversal(root.Left), preorderTraversal(root.Right)...)...)
+}
