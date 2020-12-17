bbae2923ea4d2e76c8046d37811c63d3f96fd6f4
Tai <letientai299@gmail.com>
2020-12-17 15:27:34 +0800

    Done go 300

---
+++ go/300.longest-increasing-subsequence.go
@@ -0,0 +1,86 @@
+package main
+
+/*
+ * @lc app=leetcode id=300 lang=golang
+ *
+ * [300] Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (41.70%)
+ * Total Accepted:    464.2K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * Given an integer array nums, return the length of the longest strictly
+ * increasing subsequence.
+ *
+ * A subsequence is a sequence that can be derived from an array by deleting
+ * some or no elements without changing the order of the remaining elements.
+ * For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+ * the length is 4.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 2500
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ *
+ * Follow up:
+ *
+ *
+ * Could you come up with the O(n^2) solution?
+ * Could you improve it to O(n log(n)) time complexity?
+ *
+ *
+ */
+
+// TODO (tai): can be faster
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = 1
+	best := 1
+	for i := 1; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] && dp[i] <= dp[j] {
+				dp[i] = dp[j] + 1
+			}
+		}
+		if dp[i] > best {
+			best = dp[i]
+		}
+	}
+	return best
+}
---
+++ go/300.longest-increasing-subsequence_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_lengthOfLIS(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{nums: []int{0,1,0,3,2,3}, want: 4},
+		{nums: []int{1, 3, 6, 7, 9, 4, 10, 5, 6}, want: 6},
+		{nums: []int{10, 9, 2, 5, 3, 7, 101, 18}, want: 4},
+		{nums: []int{7, 7, 7, 7, 7, 7, 7}, want: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLIS(tt.nums); got != tt.want {
+				t.Errorf("lengthOfLIS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
