38b1bf8bc5f92e55d3dde1ae9a15f0181aa7995f
Tai <letientai299@gmail.com>
2020-12-27 03:58:19 +0800

    Done go 200

---
+++ go/200.number-of-islands.go
@@ -0,0 +1,170 @@
+package main
+
+/*
+ * @lc app=leetcode id=200 lang=golang
+ *
+ * [200] Number of Islands
+ *
+ * https://leetcode.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (43.78%)
+ * Total Accepted:    905.5K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * Given an m x n 2d grid map of '1's (land) and '0's (water), return the
+ * number of islands.
+ *
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are
+ * all surrounded by water.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [
+ * ⁠ ["1","1","1","1","0"],
+ * ⁠ ["1","1","0","1","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: grid = [
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","1","0","0"],
+ * ⁠ ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ *
+ *
+ */
+
+// TODO: 0ms, but we're slower than others
+func numIslands(grid [][]byte) int {
+	rows := len(grid)
+	if rows == 0 {
+		return 0
+	}
+	cols := len(grid[0])
+	if cols == 0 {
+		return 0
+	}
+
+	type segment []int // start, end, id
+	var prevSegs []segment
+	var cnt int
+
+	id := 1
+	for r := 0; r < rows; r++ {
+		row := grid[r]
+		cur := []int{-1, -1, 0}
+		pre := byte('0')
+
+		var segs []segment
+
+		for c := 0; c < cols; c++ {
+			if row[c] == pre {
+				continue
+			}
+
+			if pre == '0' {
+				cur[0] = c // found new segment
+			} else {
+				cur[1] = c - 1 // close current segment
+				segs = append(segs, cur)
+				cur = []int{-1, -1, 0}
+			}
+			pre = row[c]
+		}
+
+		if cur[1] == -1 && cur[0] != -1 {
+			cur[1] = cols - 1
+			segs = append(segs, cur)
+		}
+
+		connected := make(map[int]int)
+		c, p := 0, 0
+		for ; c < len(segs) && p < len(prevSegs); {
+			prev := prevSegs[p]
+			cur = segs[c]
+
+			if !(prev[1] < cur[0] || cur[1] < prev[0]) {
+				if cur[2] == 0 || cur[2] > prev[2] {
+					old := cur[2]
+					cur[2] = prev[2] // same id
+					if old != 0 {
+						for j := c - 1; j >= 0; j-- {
+							if segs[j][2] == old {
+								segs[j][2] = cur[2]
+							}
+						}
+					}
+				} else {
+					old := prev[2]
+					prev[2] = cur[2]
+					for j := p + 1; j < len(prevSegs); j++ {
+						if prevSegs[j][2] == old {
+							prevSegs[j][2] = prev[2]
+						}
+					}
+				}
+				connected[prev[2]] = 1
+			}
+
+			if prev[1] < cur[1] {
+				p++
+			} else {
+				if cur[2] == 0 {
+					cur[2] = id
+					id++
+				}
+				c++
+			}
+		}
+
+		for p := 0; p < len(prevSegs); p++ {
+			if connected[prevSegs[p][2]] == 0 {
+				cnt++
+				connected[prevSegs[p][2]]++
+			}
+		}
+
+		for c < len(segs) {
+			if segs[c][2] == 0 {
+				segs[c][2] = id
+				id++
+			}
+			c++
+		}
+
+		prevSegs = segs
+	}
+
+	candidates := make(map[int]int)
+	for _, cur := range prevSegs {
+		if candidates[cur[2]] == 0 {
+			candidates[cur[2]]++
+			cnt++
+		}
+	}
+
+	return cnt
+}
---
+++ go/200.number-of-islands_test.go
@@ -0,0 +1,179 @@
+package main
+
+import "testing"
+
+type numIslandTestCase struct {
+	name string
+	grid []string
+	want int
+}
+
+func getNumIslandTests() []numIslandTestCase {
+	return []numIslandTestCase{
+		{
+			grid:
+			[]string{
+				"10110010111101011110",
+				"01001010111111011011",
+				"10010101011011100110",
+				"01100110111100100011",
+				"11010010001010111011",
+				"00001011001001011110",
+				"10111101101101110010",
+				"01100010010111001101",
+				"00001101001101001010",
+				"00111010101110111110",
+				"10101110111010101011",
+				"00111101110100011101",
+				"11100000110111011110",
+				"00111001001111110110",
+				"00011000011010011111",
+				"01110100111110111001",
+				"00001111000010000110",
+				"11111111110110111111",
+				"01001001111110101111",
+				"00111110001111110110",
+			},
+			want: 23,
+		},
+
+		{
+			grid:
+			[]string{
+				"111111111",
+				"100000001",
+				"101010101",
+				"101111101",
+				"101010101",
+				"100000001",
+				"111111111",
+			},
+
+			want: 2,
+		},
+
+
+		{
+			grid:
+			[]string{
+				"10111",
+				"10101",
+				"11101",
+			},
+			want: 1,
+		},
+
+		{
+			grid:
+			[]string{
+				"10001",
+				"01000",
+				"00101",
+				"00010",
+			},
+			want: 6,
+		},
+
+		{
+			grid:
+			[]string{
+				"11000",
+				"11000",
+				"00100",
+				"00011",
+			},
+			want: 3,
+		},
+
+		{
+			grid: []string{
+				"11000110000",
+				"01101100000",
+				"00111000000",
+				"01000000000",
+				"01100000000",
+				"01100001000",
+			},
+			want: 3,
+		},
+	}
+}
+
+func Test_numIslands(t *testing.T) {
+	tests := getNumIslandTests()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bs := make([][]byte, 0, len(tt.grid))
+			for _, s := range tt.grid {
+				bs = append(bs, []byte(s))
+			}
+
+			if got := numIslands(bs); got != tt.want {
+				t.Errorf("numIslands() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark_numIslands(b *testing.B) {
+	tests := getNumIslandTests()
+
+	bss := make([][][]byte, 0, len(tests))
+	for _, ss := range tests {
+		bs := make([][]byte, 0, len(ss.grid))
+		for _, s := range ss.grid {
+			bs = append(bs, []byte(s))
+		}
+		bss = append(bss, bs)
+	}
+
+	benchs := []struct {
+		fn   func(grid [][]byte) int
+		name string
+	}{
+		{name: "mine", fn: numIslands},
+		{name: "other", fn: numIslands_1},
+	}
+
+	for _, ben := range benchs {
+		b.Run(ben.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				for _, bs := range bss {
+					ben.fn(bs)
+				}
+			}
+		})
+	}
+}
+
+func dfs_numIslands_1(grid [][]byte, row, col int) bool {
+	if grid[row][col] == '1' {
+		grid[row][col] = '0'
+		if row+1 < len(grid) {
+			dfs_numIslands_1(grid, row+1, col)
+		}
+		if row-1 >= 0 {
+			dfs_numIslands_1(grid, row-1, col)
+		}
+		if col+1 < len(grid[0]) {
+			dfs_numIslands_1(grid, row, col+1)
+		}
+		if col-1 >= 0 {
+			dfs_numIslands_1(grid, row, col-1)
+		}
+		return true
+	}
+	return false
+}
+
+func numIslands_1(grid [][]byte) int {
+	count := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if dfs_numIslands_1(grid, i, j) {
+				count++
+			}
+		}
+	}
+	return count
+}
