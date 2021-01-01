2b6bd5e65d0654d43c413ab080073b1286390ee7
Tai <letientai299@gmail.com>
2021-01-02 02:38:39 +0800

    Done go 498

---
+++ go/498.diagonal-traverse.go
@@ -0,0 +1,97 @@
+package main
+
+/*
+ * @lc app=leetcode id=498 lang=golang
+ *
+ * [498] Diagonal Traverse
+ *
+ * https://leetcode.com/problems/diagonal-traverse/description/
+ *
+ * algorithms
+ * Medium (46.34%)
+ * Total Accepted:    114.2K
+ * Total Submissions: 228.1K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * Given a matrix of M x N elements (M rows, N columns), return all elements of
+ * the matrix in diagonal order as shown in the below image.
+ *
+ *
+ *
+ * Example:
+ *
+ *
+ * Input:
+ * [
+ * ⁠[ 1, 2, 3 ],
+ * ⁠[ 4, 5, 6 ],
+ * ⁠[ 7, 8, 9 ]
+ * ]
+ *
+ * Output:  [1,2,4,7,5,3,6,8,9]
+ *
+ * Explanation:
+ *
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ * The total number of elements of the given matrix will not exceed 10,000.
+ *
+ */
+
+func findDiagonalOrder(mat [][]int) []int {
+	m := len(mat)
+	if m == 0 {
+		return nil
+	}
+
+	if m == 1 {
+		return mat[0]
+	}
+	n := len(mat[0])
+
+	if n == 0 {
+		return nil
+	}
+
+	res := make([]int, 0, m*n)
+	up := true
+	y, x := 0, 0
+	dx, dy := 1, -1
+
+	for len(res) < m*n {
+		for 0 <= x && x < n && 0 <= y && y < m {
+			res = append(res, mat[y][x])
+			x += dx
+			y += dy
+		}
+
+		up = !up
+		dx, dy = dy, dx
+		switch {
+		case x < 0:
+			x = 0
+			if y >= m {
+				y = m - 1
+				x++
+			}
+		case y < 0:
+			y = 0
+			if x >= n {
+				x = n - 1
+				y++
+			}
+		case x >= n:
+			x = n - 1
+			y += 2
+		case y >= m:
+			y = m - 1
+			x += 2
+		}
+	}
+
+	return res
+}
---
+++ go/498.diagonal-traverse_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findDiagonalOrder(t *testing.T) {
+	tests := []struct {
+		name string
+		mat  [][]int
+		want []int
+	}{
+		{
+			mat: [][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+			},
+			want: []int{1, 2, 4, 7, 5, 3, 6, 8, 9},
+		},
+
+		{
+			mat: [][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+			},
+			want: []int{1, 2, 4, 5, 3, 6},
+		},
+
+		{
+			mat: [][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+				{10, 11, 12},
+			},
+			want: []int{1, 2, 4, 7, 5, 3, 6, 8, 10, 11, 9, 12},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findDiagonalOrder(tt.mat); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findDiagonalOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
