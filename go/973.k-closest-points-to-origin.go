3081539b70d3fcfef293de354e2998eae14cd6bc
Tai <letientai299@gmail.com>
2021-01-07 02:23:22 +0800

    done go 973

---
+++ go/973.k-closest-points-to-origin.go
@@ -0,0 +1,83 @@
+package main
+
+import "sort"
+
+/*
+ * @lc app=leetcode id=973 lang=golang
+ *
+ * [973] K Closest Points to Origin
+ *
+ * https://leetcode.com/problems/k-closest-points-to-origin/description/
+ *
+ * algorithms
+ * Medium (61.40%)
+ * Total Accepted:    393.1K
+ * Total Submissions: 610.1K
+ * Testcase Example:  '[[1,3],[-2,2]]\n1'
+ *
+ * We have a list of points on the plane.  Find the K closest points to the
+ * origin (0, 0).
+ *
+ * (Here, the distance between two points on a plane is the Euclidean
+ * distance.)
+ *
+ * You may return the answer in any order.  The answer is guaranteed to be
+ * unique (except for the order that it is in.)
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: points = [[1,3],[-2,2]], K = 1
+ * Output: [[-2,2]]
+ * Explanation:
+ * The distance between (1, 3) and the origin is sqrt(10).
+ * The distance between (-2, 2) and the origin is sqrt(8).
+ * Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+ * We only want the closest K = 1 points from the origin, so the answer is just
+ * [[-2,2]].
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+ * Output: [[3,3],[-2,4]]
+ * (The answer [[-2,4],[3,3]] would also be accepted.)
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * 1 <= K <= points.length <= 10000
+ * -10000 < points[i][0] < 10000
+ * -10000 < points[i][1] < 10000
+ *
+ *
+ *
+ */
+
+// quick select is the best solution for this kind of problem,
+// but I just don't want to implement it again.
+func kClosest(points [][]int, k int) [][]int {
+	n := len(points)
+	if n <= k {
+		return points
+	}
+
+	dis := func(x []int) int {
+		return x[0]*x[0] + x[1]*x[1]
+	}
+
+	sort.Slice(points, func(i, j int) bool {
+		a, b := points[i], points[j]
+		return dis(a) < dis(b)
+	})
+
+	return points[:k]
+}
