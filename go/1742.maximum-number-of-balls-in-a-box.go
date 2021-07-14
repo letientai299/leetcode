8f32c2ee2c85bda384de93054c183385389fd408
Tai <letientai299@gmail.com>
2021-07-15 03:12:30 +0700

    Done go 1742

---
+++ go/1742.maximum-number-of-balls-in-a-box.go
@@ -0,0 +1,87 @@
+package main
+
+// Maximum Number of Balls in a Box
+//
+// Easy
+//
+// https://leetcode.com/problems/maximum-number-of-balls-in-a-box/
+//
+// You are working in a ball factory where you have `n` balls numbered from
+// `lowLimit` up to `highLimit` **inclusive** (i.e., `n == highLimit - lowLimit
+// + 1`), and an infinite number of boxes numbered from `1` to `infinity`.
+//
+// Your job at this factory is to put each ball in the box with a number equal
+// to the sum of digits of the ball's number. For example, the ball number `321`
+// will be put in the box number `3 + 2 + 1 = 6` and the ball number `10` will
+// be put in the box number `1 + 0 = 1`.
+//
+// Given two integers `lowLimit` and `highLimit`, return _the number of balls in
+// the box with the most balls._
+//
+// **Example 1:**
+//
+// ```
+// Input: lowLimit = 1, highLimit = 10
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+// Ball Count:  2 1 1 1 1 1 1 1 1 0  0  ...
+// Box 1 has the most number of balls with 2 balls.
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: lowLimit = 5, highLimit = 15
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+// Ball Count:  1 1 1 1 2 2 1 1 1 0  0  ...
+// Boxes 5 and 6 have the most number of balls with 2 balls in each.
+//
+// ```
+//
+// **Example 3:**
+//
+// ```
+// Input: lowLimit = 19, highLimit = 28
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 12 ...
+// Ball Count:  0 1 1 1 1 1 1 1 1 2  0  0  ...
+// Box 10 has the most number of balls with 2 balls.
+//
+// ```
+//
+// **Constraints:**
+//
+// - `1 <= lowLimit <= highLimit <= 105`
+func countBalls(lowLimit int, highLimit int) int {
+	boxes := [46]int{} // limit is 10^5
+	m := 0
+
+	v := 0
+	n := lowLimit
+	for n > 0 {
+		v += n % 10
+		n /= 10
+	}
+	boxes[v]++
+	m = boxes[v]
+
+	for i := lowLimit + 1; i <= highLimit; i++ {
+		v++
+		n = i
+		for n%10 == 0 {
+			v -= 9
+			n /= 10
+		}
+		boxes[v]++
+		if boxes[v] > m {
+			m = boxes[v]
+		}
+	}
+	return m
+}
+
+// Good for interview
