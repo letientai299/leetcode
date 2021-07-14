16a5063debf023beef99f1db76e755748b24a07f
Tai <letientai299@gmail.com>
2021-07-15 02:40:41 +0700

    Done go 1720

---
+++ go/1720.decode-xored-array.go
@@ -0,0 +1,53 @@
+package main
+
+// Decode XORed Array
+//
+// Easy
+//
+// https://leetcode.com/problems/decode-xored-array/
+//
+// There is a **hidden** integer array `arr` that consists of `n` non-negative
+// integers.
+//
+// It was encoded into another integer array `encoded` of length `n - 1`, such
+// that `encoded[i] = arr[i] XOR arr[i + 1]`. For example, if `arr = [1,0,2,1]`,
+// then `encoded = [1,2,3]`.
+//
+// You are given the `encoded` array. You are also given an integer `first`,
+// that is the first element of `arr`, i.e. `arr[0]`.
+//
+// Return _the original array_ `arr`. It can be proved that the answer exists
+// and is unique.
+//
+// **Example 1:**
+//
+// ```
+// Input: encoded = [1,2,3], first = 1
+// Output: [1,0,2,1]
+// Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR
+// 2, 2 XOR 1] = [1,2,3]
+//
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: encoded = [6,2,7,3], first = 4
+// Output: [4,2,0,7,4]
+//
+// ```
+//
+// **Constraints:**
+//
+// - `2 <= n <= 104`
+// - `encoded.length == n - 1`
+// - `0 <= encoded[i] <= 105`
+// - `0 <= first <= 105`
+func decode(encoded []int, first int) []int {
+	res := make([]int, len(encoded)+1)
+	res[0] = first
+	for i, v := range encoded {
+		res[i+1] = res[i] ^ v
+	}
+	return res
+}
