f18c1a62cf40024da5b9967689abb6b649f5acff
Tai <letientai299@gmail.com>
2021-07-17 03:47:37 +0700

    Done go 1779

---
+++ go/1779.find-nearest-point-that-has-the-same-x-or-y-coordinate.go
@@ -0,0 +1,91 @@
+package main
+
+import "math"
+
+// Find Nearest Point That Has the Same X or Y Coordinate
+//
+// Easy
+//
+// https://leetcode.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+//
+// You are given two integers, `x` and `y`, which represent your current
+// location on a Cartesian grid: `(x, y)`. You are also given an array `points`
+// where each `points[i] = [ai, bi]` represents that a point exists at `(ai,
+// bi)`. A point is **valid** if it shares the same x-coordinate or the same
+// y-coordinate as your location.
+//
+// Return _the index **(0-indexed)** of the **valid** point with the smallest
+// **Manhattan distance** from your current location_. If there are multiple,
+// return _the valid point with the **smallest** index_. If there are no valid
+// points, return `-1`.
+//
+// The **Manhattan distance** between two points `(x1, y1)` and `(x2, y2)` is
+// `abs(x1 - x2) + abs(y1 - y2)`.
+//
+// **Example 1:**
+//
+// ```
+// Input: x = 3, y = 4, points = [[1,2],[3,1],[2,4],[2,3],[4,4]]
+// Output: 2
+// Explanation: Of all the points, only [3,1], [2,4] and [4,4] are valid. Of the
+// valid points, [2,4] and [4,4] have the smallest Manhattan distance from your
+// current location, with a distance of 1. [2,4] has the smallest index, so
+// return 2.
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: x = 3, y = 4, points = [[3,4]]
+// Output: 0
+// Explanation: The answer is allowed to be on the same location as your current
+// location.
+// ```
+//
+// **Example 3:**
+//
+// ```
+// Input: x = 3, y = 4, points = [[2,3]]
+// Output: -1
+// Explanation: There are no valid points.
+// ```
+//
+// **Constraints:**
+//
+// - `1 <= points.length <= 104`
+// - `points[i].length == 2`
+// - `1 <= x, y, ai, bi <= 104`
+func nearestValidPoint(x int, y int, points [][]int) int {
+	res := -1
+	min := math.MaxInt32
+
+	abs := func(n int) int {
+		if n > 0 {
+			return n
+		}
+		return -n
+	}
+
+	for i, p := range points {
+		if x != p[0] && y != p[1] {
+			continue // invalid
+		}
+
+		if x == p[0] {
+			v := abs(p[1] - y)
+			if min > v {
+				min = v
+				res = i
+			}
+			continue
+		}
+
+		v := abs(p[0] - x)
+		if min > v {
+			min = v
+			res = i
+		}
+	}
+
+	return res
+}
