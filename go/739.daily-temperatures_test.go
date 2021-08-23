ede5a1921282c3ac332a3b9cdbddbe6bdbd4b3e5
Tai <tai.le@shopee.com>
2021-08-23 23:06:54 +0700

    Done go 739

---
+++ go/739.daily-temperatures_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_dailyTemperatures(t *testing.T) {
+	tests := []struct {
+		name         string
+		temperatures []int
+		want         []int
+	}{
+		{
+			temperatures: []int{73, 74, 75, 71, 69, 72, 76, 73},
+			want:         []int{1, 1, 4, 2, 1, 1, 0, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := dailyTemperatures(tt.temperatures); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("dailyTemperatures() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
