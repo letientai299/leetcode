1ea4e8469d3ff6be41deefe9fe7c213842cba604
Tai <letientai299@gmail.com>
2021-08-29 03:13:44 +0700

    Done go 912

---
+++ go/912.sort-an-array.go
@@ -0,0 +1,36 @@
+package main
+
+import "sort"
+
+// Sort an Array
+//
+// Medium
+//
+// https://leetcode.com/problems/sort-an-array/
+//
+// Given an array of integers `nums`, sort the array in ascending order.
+//
+// **Example 1:**
+//
+// ```
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+//
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: nums = [5,1,1,2,0,0]
+// Output: [0,0,1,1,2,5]
+//
+// ```
+//
+// **Constraints:**
+//
+// - `1 <= nums.length <= 5 * 104`
+// - `-5 * 104 <= nums[i] <= 5 * 104`
+func sortArray(nums []int) []int {
+	sort.Ints(nums)
+	return nums
+}
