dbbec9f0bd348e703a6605d4e0ae43129d5ce65f
Tai <letientai299@gmail.com>
2021-08-29 12:58:47 +0700

    Done go 915

---
+++ go/915.partition-array-into-disjoint-intervals.go
@@ -0,0 +1,74 @@
+package main
+
+import "math"
+
+// Partition Array into Disjoint Intervals
+//
+// Medium
+//
+// https://leetcode.com/problems/partition-array-into-disjoint-intervals/
+//
+// Given an integer array `nums`, partition it into two (contiguous) subarrays
+// `left` and `right` so that:
+//
+// - Every element in `left` is less than or equal to every element in `right`.
+// - `left` and `right` are non-empty.
+// - `left` has the smallest possible size.
+//
+// Return _the length of_ `left` _after such a partitioning_.
+//
+// Test cases are generated such that partitioning exists.
+//
+// **Example 1:**
+//
+// ```
+// Input: nums = [5,0,3,8,6]
+// Output: 3
+// Explanation: left = [5,0,3], right = [8,6]
+//
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: nums = [1,1,1,0,6,12]
+// Output: 4
+// Explanation: left = [1,1,1,0], right = [6,12]
+//
+// ```
+//
+// **Constraints:**
+//
+// - `2 <= nums.length <= 105`
+// - `0 <= nums[i] <= 106`
+// - There is at least one valid answer for the given input.
+func partitionDisjoint(nums []int) int {
+	n := len(nums)
+	max := make([]int, n)
+	max[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > max[i-1] {
+			max[i] = nums[i]
+		} else {
+			max[i] = max[i-1]
+		}
+	}
+
+	best := math.MaxInt32
+	min := nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		if min >= max[i] {
+			if best > i+1 {
+				best = i + 1
+			}
+		}
+
+		if min > nums[i] {
+			min = nums[i]
+		}
+	}
+
+	return best
+}
+
+// TODO (tai): slow, 9%, 2 passes. This can be done with 1 pass, O(1) space
---
+++ go/915.partition-array-into-disjoint-intervals_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_partitionDisjoint(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{
+			nums: []int{5, 0, 3, 8, 6},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := partitionDisjoint(tt.nums); got != tt.want {
+				t.Errorf("partitionDisjoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
