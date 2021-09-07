9f8e0ccdd3ccf9bc1a5861628fddee48a1ac18c3
Tai <letientai299@gmail.com>
2021-09-08 02:10:08 +0700

    Attempt 137

---
+++ go/137.single-number-ii_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_singleNumber(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{nums: []int{74, 88, 74, 14, 486, 88, 14, 74, 14, 88}, want: 486},
+		{nums: []int{0, 1, 0, 1, 0, 1, 99}, want: 99},
+		{nums: []int{2, 2, 3, 2}, want: 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+	/*
+		rand.Seed(time.Now().Unix())
+		n := rand.Int() % 10
+		arr := make([]int, 0, 3*n+1)
+		for i := 0; i < n; i++ {
+			r := rand.Int() % 100
+			arr = append(arr, r, r, r)
+		}
+
+		r := rand.Int() % 1000
+		arr = append(arr, r)
+		rand.Shuffle(len(arr), func(i, j int) { arr[i], arr[j] = arr[j], arr[i] })
+
+		got := singleNumber(arr)
+		assert.Equalf(t, got, r, "arr=%v, want=%v", arr, r)
+	*/
+}
