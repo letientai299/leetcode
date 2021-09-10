3b9bd46ee6a043f1325149a61843398fea8cbd17
Tai <letientai299@gmail.com>
2021-09-10 16:14:27 +0700

    Done go 1616

---
+++ go/1616.split-two-strings-to-make-palindrome.go
@@ -0,0 +1,107 @@
+package main
+
+// Split Two Strings to Make Palindrome
+//
+// Medium
+//
+// https://leetcode.com/problems/split-two-strings-to-make-palindrome/
+//
+// You are given two strings `a` and `b` of the same length. Choose an index and
+// split both strings **at the same index**, splitting `a` into two strings:
+// `aprefix` and `asuffix` where `a = aprefix + asuffix`, and splitting `b` into
+// two strings: `bprefix` and `bsuffix` where `b = bprefix + bsuffix`. Check if
+// `aprefix + bsuffix` or `bprefix + asuffix` forms a palindrome.
+//
+// When you split a string `s` into `sprefix` and `ssuffix`, either `ssuffix` or
+// `sprefix` is allowed to be empty. For example, if `s = "abc"`, then `"" +
+// "abc"`, `"a" + "bc"`, `"ab" + "c"` , and `"abc" + ""` are valid splits.
+//
+// Return `true` _if it is possible to form_ _a palindrome string, otherwise
+// return_ `false`.
+//
+// **Notice** that `x + y` denotes the concatenation of strings `x` and `y`.
+//
+// **Example 1:**
+//
+// ```
+// Input: a = "x", b = "y"
+// Output: true
+// Explaination: If either a or b are palindromes the answer is true since you
+// can split in the following way:
+// aprefix = "", asuffix = "x"
+// bprefix = "", bsuffix = "y"
+// Then, aprefix + bsuffix = "" + "y" = "y", which is a palindrome.
+//
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: a = "abdef", b = "fecab"
+// Output: true
+//
+// ```
+//
+// **Example 3:**
+//
+// ```
+// Input: a = "ulacfd", b = "jizalu"
+// Output: true
+// Explaination: Split them at index 3:
+// aprefix = "ula", asuffix = "cfd"
+// bprefix = "jiz", bsuffix = "alu"
+// Then, aprefix + bsuffix = "ula" + "alu" = "ulaalu", which is a palindrome.
+//
+// ```
+//
+// **Example 4:**
+//
+// ```
+// Input: a = "xbdef", b = "xecab"
+// Output: false
+//
+// ```
+//
+// **Constraints:**
+//
+// - `1 <= a.length, b.length <= 105`
+// - `a.length == b.length`
+// - `a` and `b` consist of lowercase English letters
+func checkPalindromeFormation(a string, b string) bool {
+	var isPalindrome func(l, r int, left, right string, switched bool) bool
+
+	isPalindrome = func(l, r int, left, right string, canSwitch bool) bool {
+		if l >= r {
+			return true
+		}
+
+		if left[l] == right[r] {
+			return isPalindrome(l+1, r-1, left, right, canSwitch)
+		}
+
+		if !canSwitch {
+			return false
+		}
+
+		return isPalindrome(l, r, left, left, !canSwitch) ||
+			isPalindrome(l, r, right, right, !canSwitch)
+	}
+
+	n := len(a)
+	if isPalindrome(0, n-1, a, a, false) ||
+		isPalindrome(0, n-1, b, b, false) {
+		return true
+	}
+
+	if isPalindrome(0, n-1, a, b, true) {
+		return true
+	}
+
+	if isPalindrome(0, n-1, b, a, true) {
+		return true
+	}
+
+	return false
+}
+
+// Good for interview
