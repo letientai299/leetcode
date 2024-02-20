3d52c6a6a06978f46e779a9c3291f8a895e64c1a
Tai <letientai299@gmail.com>
2024-02-21 00:27:17 +0700

    Simplify rust 2309 and introduce test-case crate

--- rust/src/p_2309_greatest_letter.rs
+++ rust/src/p_2309_greatest_letter.rs
@@ -3,48 +3,38 @@ struct Solution {}
 
 impl Solution {
     pub fn greatest_letter(s: String) -> String {
-        let mut all = [0; 26];
-        let mut best: Option<u8> = None;
+        let mut alpha = [0; 26];
+        let mut best = None;
 
-        s.chars().for_each(|c| {
-            let i = if c.is_ascii_lowercase() {
-                all[(c as usize) - 'a' as usize] |= 1;
-                (c as u8) - b'a'
-            } else {
-                all[(c as usize) - 'A' as usize] |= 2;
-                (c as u8) - b'A'
-            };
-
-            let v = all[i as usize];
-            if v == 3 && (best.is_none() || best.is_some_and(|index| index < i))
-            {
+        for c in s.chars() {
+            let (i, v) = Self::consider(&mut alpha, c);
+            if v == 3 && (best.is_none() || best.unwrap() < i) {
                 best = Some(i)
             }
-        });
-
-        match best {
-            None => "".to_string(),
-            Some(index) => ((index + b'A') as char).to_string(),
         }
+
+        best.map_or("".into(), |c| char::from(c + b'A').to_string())
+    }
+    fn consider(alpha: &mut [i32; 26], c: char) -> (u8, i32) {
+        let i = (c.to_ascii_lowercase() as u8 - b'a') as usize;
+        alpha[i] |= if c.is_ascii_lowercase() { 1 } else { 2 };
+        (i as u8, alpha[i])
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
-    #[test]
-    fn greatest_letter() {
-        let tests = [("lEeTcOdE", "E")];
-
-        for tc in tests {
-            let (input, want) = tc;
-            let actual = Solution::greatest_letter(input.into());
-            assert_eq!(
-                want, actual,
-                "input={:?}, want={:?}, actual={:?}",
-                input, want, actual
-            );
-        }
+    #[test_case("lEeTcOdE", "E")]
+    #[test_case("AbCdEfGhIjK", "")]
+    fn greatest_letter(input: &str, want: &str) {
+        let actual = Solution::greatest_letter(input.into());
+        assert_eq!(
+            want, actual,
+            "input={:?}, want={:?}, actual={:?}",
+            input, want, actual
+        );
     }
 }
