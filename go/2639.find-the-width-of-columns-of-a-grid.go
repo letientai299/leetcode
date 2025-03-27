92c31c6bda08ccab8761c600b61ceac1470a36b5
Tai <letientai299@gmail.com>
2025-03-27 23:24:15 +0700

    Done go 2639

---
+++ go/2639.find-the-width-of-columns-of-a-grid.go
@@ -0,0 +1,74 @@
+package main
+
+// Find the Width of Columns of a Grid
+//
+// Easy
+//
+// https://leetcode.com/problems/find-the-width-of-columns-of-a-grid
+//
+// You are given a **0-indexed** `m x n` integer matrix `grid`. The width of a
+// column is the maximum **length** of its integers.
+//
+// - For example, if `grid = [[-10], [3], [12]]`, the width of the only column
+// is `3` since `-10` is of length `3`.
+//
+// Return _an integer array_ `ans` _of size_ `n` _where_ `ans[i]` _is the width
+// of the_ `ith` _column_.
+//
+// The **length** of an integer `x` with `len` digits is equal to `len` if `x`
+// is non-negative, and `len + 1` otherwise.
+//
+// **Example 1:**
+//
+// ```
+// Input: grid = [[1],[22],[333]]
+// Output: [3]
+// Explanation: In the 0th column, 333 is of length 3.
+//
+// ```
+//
+// **Example 2:**
+//
+// ```
+// Input: grid = [[-15,1,3],[15,7,12],[5,6,-2]]
+// Output: [3,1,2]
+// Explanation:
+// In the 0th column, only -15 is of length 3.
+// In the 1st column, all integers are of length 1.
+// In the 2nd column, both 12 and -2 are of length 2.
+//
+// ```
+//
+// **Constraints:**
+//
+// - `m == grid.length`
+// - `n == grid[i].length`
+// - `1 <= m, n <= 100 `
+// - `-109 <= grid[r][c] <= 109`
+func findColumnWidth(grid [][]int) []int {
+  m := len(grid)
+  n := len(grid[0])
+  ans := make([]int, n)
+  for r := 0; r < m; r++ {
+    for c := 0; c < n; c++ {
+      x := grid[r][c]
+      if x == 0 {
+        ans[c] = max(ans[c], 1)
+        continue
+      }
+
+      w := 0
+      for y := x; y != 0; y /= 10 {
+        w++
+      }
+
+      if x < 0 {
+        w++
+      }
+
+      ans[c] = max(ans[c], w)
+    }
+  }
+
+  return ans
+}
